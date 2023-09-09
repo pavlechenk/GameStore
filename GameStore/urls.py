c267976481351c86c67708693a8b927e35149bac
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-09-10 01:53:32 +0300

    Add index and games functions to the view.py and implement url routing

--- GameStore/urls.py
+++ GameStore/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from games.views import index, games
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('games/', games, name='games'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(requests):
+    context = {
+        'title': 'Store'
+    }
+
+    return render(requests, 'games/index.html', context)
+
+
+def games(requests):
+    context = {
+        'title': 'Store - Каталог'
+    }
+
+    return render(requests, 'games/games.html', context)
