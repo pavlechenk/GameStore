ebf817a74a6eef6ee3c7de8eeae1708c54b4b1ee
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-09-10 03:19:19 +0300

    Create migration files and classes that describe the data model, apply the created migrations

--- games/models.py
+++ games/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class GameGenres(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='games_images')
+    genre = models.ForeignKey(to=GameGenres, on_delete=models.CASCADE)
+
