40d7c65e2efb5922a047dd9510234f730f57a58d
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-09-19 01:37:18 +0300

    I create a UserLoginForm form with the username and password fields, implement the authorization logic and add a catatom form template

--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(requests):
-    return render(requests, 'users/login.html')
+    if requests.method == 'POST':
+        form = UserLoginForm(data=requests.POST)
+        if form.is_valid():
+            username = requests.POST['username']
+            password = requests.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(requests, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    context = {
+        'form': UserLoginForm(),
+    }
+
+    return render(requests, 'users/login.html', context)
 
 
 def registration(requests):
