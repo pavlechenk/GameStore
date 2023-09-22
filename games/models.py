b46c93849aa1c3d39b40e1b1483a63f7f04e445e
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-09-22 17:14:54 +0300

    Creating a basket model and applying migration files

--- games/models.py
+++ games/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from users.models import User
 
 
 class GameGenres(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Game(models.Model):
@@ -14,3 +18,15 @@ class Game(models.Model):
     image = models.ImageField(upload_to='games_images')
     genre = models.ForeignKey(to=GameGenres, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Игра: {self.name} | Жанр: {self.genre}"
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    game = models.ForeignKey(to=Game, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Корзина для {self.user.username} | Продукт: {self.game.name}"
