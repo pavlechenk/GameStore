0acc218ac19555f90a5ef1fb44aa6567dcbdfc53
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-09 08:19:15 +0300

    Implementing url routing for an order creation template

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'orders'
+    'orders',
     'games',
     'users',
 ]
--- GameStore/urls.py
+++ GameStore/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('games/', include('games.urls', namespace='games')),
     path('users/', include('users.urls', namespace='users')),
+    path('orders/', include('orders.urls', namespace='orders'))
 ]
 
 if settings.DEBUG:
