c4c7bc12249477d82765c6f197b430d5076aafed
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-11 13:16:06 +0300

    I connect stripe and create a checkout session

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from orders.views import OrderCreateView
+from orders.views import OrderCreateView, SuccessTemplateView, CanceledTemplateView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order_create/', OrderCreateView.as_view(), name='order_create'),
+    path('order_success/', SuccessTemplateView.as_view(), name='order_success'),
+    path('order_canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
