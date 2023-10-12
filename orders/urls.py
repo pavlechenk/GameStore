e824b174f067a5314f92fc008ab78487ae665919
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-12 05:36:03 +0300

    Creating order detail pages

--- orders/urls.py
+++ orders/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from orders.views import (CanceledTemplateView, OrderCreateView, OrderListView,
-                          SuccessTemplateView)
+from orders.views import (CanceledTemplateView, OrderCreateView,
+                          OrderDetailView, OrderListView, SuccessTemplateView)
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order_create/', OrderCreateView.as_view(), name='order_create'),
     path('', OrderListView.as_view(), name='orders_list'),
+    path('order/<int:pk>/', OrderDetailView.as_view(), name='order'),
     path('order_success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order_canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
