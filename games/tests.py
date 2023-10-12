db86279083922b0933af575e6d9e03a8bfc78058
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-12 06:31:39 +0300

    Implementing basic tests

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from games.models import Game, GameGenres
+
+
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'GameStore')
+        self.assertTemplateUsed(response, 'games/index.html')
+
+
+class GamesListViewTestCase(TestCase):
+    fixtures = ['games.json', 'genres.json']
+
+    def setUp(self):
+        self.games = Game.objects.all()
+
+    def test_list(self):
+        path = reverse('games:index')
+        response = self.client.get(path)
+
+        self.__common_test(response)
+        self.assertEqual(list(response.context_data['games']), list(self.games[:3]))
+
+    def test_list_with_category(self):
+        genre = GameGenres.objects.first()
+        path = reverse('games:genre', kwargs={'genre_id': genre.id})
+        response = self.client.get(path)
+
+        self.__common_test(response)
+        self.assertEqual(
+            list(response.context_data['games']),
+            list(self.games.filter(genre_id=genre.id))
+        )
+
+    def __common_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'GameStore - Каталог')
+        self.assertTemplateUsed(response, 'games/games.html')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,95 @@
-from django.test import TestCase
+from datetime import timedelta
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from users.models import EmailVerification, User
+
+
+class UserLoginViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+        self.path = reverse('users:login')
+
+    def test_user_login_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'GameStore - Авторизация')
+        self.assertTemplateUsed(response, 'users/login.html')
+
+    def test_user_login_post(self):
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(self.path, data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_login_error(self):
+        data = {
+            'username': 'testuser',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(self.path, data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertIn('form', response.context)
+        self.assertTrue(response.context['form'].errors)
+
+
+class UserRegistrationViewTestCase(TestCase):
+    def setUp(self):
+        self.data = {
+            'first_name': 'Александр',
+            'last_name': 'Раздымахо',
+            'username': 'pavlechenk',
+            'email': 'pavlechenk@gmail.com',
+            'password1': 'A888rz08',
+            'password2': 'A888rz08',
+        }
+
+        self.path = reverse('users:registration')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'GameStore - Регистрация')
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_post(self):
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        # check creating of user
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        # check creating of email_verification
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_error(self):
+        User.objects.create(username=self.data['username'], email=self.data['email'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/registration.html')
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+        self.assertContains(response, 'Пользователь с таким Email уже существует.', html=True)
