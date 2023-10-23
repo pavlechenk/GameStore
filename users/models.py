1230fd81f8683eb745bed240a2768c60297b7221
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-23 16:51:41 +0300

    I use inheritance to implement sending different emails

--- users/models.py
+++ users/models.py
@@ -12,24 +12,21 @@ class User(AbstractUser):
     is_verified_email = models.BooleanField(default=False)
 
 
-class EmailVerification(models.Model):
+class BaseEmailSending(models.Model):
     code = models.UUIDField(unique=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     expiration = models.DateTimeField()
 
-    def __str__(self):
-        return f"EmailVerification object for {self.user.email}"
+    def __init__(self, subject, message):
+        super().__init__()
+        self.subject = subject
+        self.message = message
 
-    def send_email_verification(self):
-        url = reverse('users:email_verification', args=(self.user.email, self.code))
-        full_link = f"{settings.DOMAIN_NAME}{url}"
-        subject = f"Подтверждение учетной записи для {self.user.username}"
-        message = "Для подтверждения учетной записи для {} перейдите по ссылке {}".format(self.user.email, full_link)
-
+    def send_email(self):
         send_mail(
-            subject=subject,
-            message=message,
+            subject=self.subject,
+            message=self.message,
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[self.user.email],
             fail_silently=False,
@@ -37,3 +34,33 @@ class EmailVerification(models.Model):
 
     def is_expired(self):
         return True if now() >= self.expiration else False
+
+    def _get_full_link(self, view_name):
+        url = reverse(view_name, args=(self.user.email, self.code))
+        full_link = f"{settings.DOMAIN_NAME}{url}"
+        return full_link
+
+
+class EmailVerification(BaseEmailSending):
+    def __init__(self):
+        subject = f"Подтверждение учетной записи для {self.user.username}"
+        message = "Для подтверждения учетной записи для {} перейдите по ссылке {}".format(
+            self.user.email, self._get_full_link('users:email_verification')
+        )
+        super().__init__(subject, message)
+
+    def __str__(self):
+        return f"EmailVerification object for {self.user.email}"
+
+
+class EmailResetPassword(BaseEmailSending):
+
+    def __init__(self):
+        subject = f"Сброс пароля учетной записи {self.user.username}"
+        message = "Для сброса пароля учетной записи {} перейдите по ссылке {}".format(
+            self.user.email, self._get_full_link('users:reset_password')
+        )
+        super().__init__(subject, message)
+
+    def __str__(self):
+        return f"EmailResetPassword object for {self.user.email}"
