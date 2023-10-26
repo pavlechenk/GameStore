0143b15e3dc188acdc5539c355f20c11ab4f9ac3
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-10-26 13:56:18 +0300

    Adding a function to send a password reset email

--- users/tasks.py
+++ users/tasks.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from celery import shared_task
 from django.utils.timezone import now
 
-from users.models import EmailVerification, User
+from users.models import EmailResetPassword, EmailVerification, User
 
 
 @shared_task
@@ -12,4 +12,12 @@ def send_email_verification(user_id):
     user = User.objects.get(id=user_id)
     expiration = now() + timedelta(hours=48)
     record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+    record.send_email()
+
+
+@shared_task
+def send_email_reset_password(user_id):
+    user = User.objects.get(id=user_id)
+    expiration = now() + timedelta(hours=48)
+    record = EmailResetPassword.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
     record.send_email()
