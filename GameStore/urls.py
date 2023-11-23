d6b339f898bec925d47d1457845056a60d3b998f
Alexandr Razdymakho <pavlechenk@gmail.com>
2023-11-23 16:33:28 +0300

    Change GameStore\urls.py and add yasg.py for documentation

--- GameStore/urls.py
+++ GameStore/urls.py
@@ -17,27 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 
 from games.views import IndexView
 from orders.views import stripe_webhook_view
-
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="GameStore API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-   authentication_classes=[],
-)
+from .yasg import urlpatterns as do_urls
 
 
 urlpatterns = [
@@ -50,10 +33,9 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += do_urls
 
 if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
